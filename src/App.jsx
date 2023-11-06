994adece757f9d31164850a3bf2cbb9b6620dad0
rinaldiihsan <rinaldi.210170168@mhs.unimal.ac.id>
2023-11-06 21:15:47 +0700

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
+
+function App() {
+  return (
+    <>
+      <Header></Header>
+      <Footer></Footer>
+    </>
+  );
+}
+
+export default App;
